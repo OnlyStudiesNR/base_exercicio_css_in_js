04e5c2748d39ba662c22dc208284fd33cea1e40e
Adriano <adrianoopereiira@hotmail.com>
2024-03-31 18:20:34 -0300

    Style: converção do css para styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import StyledGlobal from './global'
 
 function App() {
   return (
     <>
+      <StyledGlobal />
       <Header />
       <Hero />
       <div className="container">
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,11 @@
-import styles from './Hero.module.css'
+import { FormHero } from './sytles'
 
 const Hero = () => (
-  <form className={styles.form}>
+  <FormHero>
     <div className="container">
-      <h2 className={styles.heroTitle}>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
+      <h2>As melhores vagas para tecnologia, design e artes visuais.</h2>
     </div>
-  </form>
+  </FormHero>
 )
 
 export default Hero
